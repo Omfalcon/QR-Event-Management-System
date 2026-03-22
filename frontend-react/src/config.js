fb0580c9608087fcbd088aff2269f05087ae2cd7
Om agarwal <black.falcon.x.69@gmail.com>
2026-03-23 02:49:24 +0530

    Change API base URL from HTTPS to HTTP

--- frontend-react/src/config.js
+++ frontend-react/src/config.js
@@ -1,5 +1,5 @@
 // API Configuration new
-export const API_BASE_URL = "https://20.244.25.244";
+export const API_BASE_URL = "http://20.244.25.244";
 
 // Helper function to get auth headers
 export const getAuthHeaders = () => {
